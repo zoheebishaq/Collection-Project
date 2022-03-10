3e9c27040427f7d3de53b9c624acc4c25a3ae526
Zoheeb Ishaque <63728270+zoheebishaq@users.noreply.github.com>
2022-03-10 21:55:28 +0100

    Initial commit

---
+++ src/collectionDemos/ArrayListDemos1.java
@@ -0,0 +1,57 @@
+package collectionDemos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemos1 {
+    public static void main(String[] args) {
+        //Declare Arraylist
+        ArrayList al = new ArrayList();
+
+        //different ArrayList
+
+//        ArrayList<Integer> alI = new ArrayList<Integer>();
+//
+//        ArrayList<String> alS = new ArrayList<String>();
+//
+//        List alList = new ArrayList();
+
+        al.add(100);
+        al.add("welcome");
+        al.add(15.5);
+        al.add('A');
+        al.add(true);
+
+        System.out.println(al); // [100, welcome, 15.5, A, true]
+
+        //size()
+        System.out.println("Number of elements in ArrayList "+ al.size()); // Number of elements in ArrayList 5
+
+        //remove
+        al.remove(1);
+      //  al.remove("welcome");
+        System.out.println("after removing element from array list:" + al); //after removing element from array list:[100, 15.5, A, true]
+
+        //insert new element
+
+        al.add(2,"bibi");
+        System.out.println("after adding new element to array list:" + al); //after adding new element to array list:[100, 15.5, bibi, A, true]
+
+        //retieve specific element
+        System.out.println(al.get(2)); //bibi
+
+        // change element/replace
+        al.set(2,"chici");
+
+        System.out.println("after replacing element" +al);//after replacing element[100, 15.5, chici, A, true]
+
+        //search -contains()
+        System.out.println(al.contains("chichi")); //false
+        System.out.println(al.contains("chici"));   // true
+
+        //isEmpty
+        System.out.println(al.isEmpty()); // false
+    }
+
+
+}
